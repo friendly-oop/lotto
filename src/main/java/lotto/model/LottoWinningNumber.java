0f5802ed36689439280b93adac7c44cbb27271dc
kkoon9 <rndrnjs2003@gmail.com>
2024-09-01 16:24:13 +0900

    feat: 로또 당첨 번호에서 중복을 체크하는 로직 개발

--- src/main/java/lotto/model/LottoWinningNumber.java
+++ src/main/java/lotto/model/LottoWinningNumber.java
@@ -2,6 +2,8 @@ package lotto.model;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static lotto.Constant.LOTTO_NUMBER_COUNT;
 import static lotto.Constant.LOTTO_WINNING_NUMBER_INPUT_EXCEPTION;
@@ -10,8 +12,11 @@ import static lotto.Constant.SPLIT_COMMA;
 public class LottoWinningNumber {
 
     public LottoWinningNumber(String input) {
-        String[] numbers = input.split(SPLIT_COMMA);
-        if (numbers.length != LOTTO_NUMBER_COUNT) {
+        Set<Integer> numbers = Arrays.stream(input.split(SPLIT_COMMA))
+            .map(Integer::parseInt)
+            .collect(Collectors.toSet());
+
+        if (numbers.size() != LOTTO_NUMBER_COUNT) {
             throw new IllegalArgumentException(LOTTO_WINNING_NUMBER_INPUT_EXCEPTION);
         }
     }
--- src/test/java/lotto/model/LottoWinningNumberTest.java
+++ src/test/java/lotto/model/LottoWinningNumberTest.java
@@ -2,15 +2,17 @@ package lotto.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static lotto.Constant.LOTTO_WINNING_NUMBER_INPUT_EXCEPTION;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoWinningNumberTest {
-    @Test
-    @DisplayName("콤마를 기준으로 나누었을 때 길이가 6인 배열이 나오지 않으면 예외를 던진다.")
-    void constructor_test1() {
-        String input = "1,2,3";
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3","1,2,2,3,4,5"})
+    @DisplayName("중복을 제외한 로또 번호 개수가 6이 아니면 예외를 던진다.")
+    void constructor_test1(String input) {
         assertThatThrownBy(() -> new LottoWinningNumber(input))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining(LOTTO_WINNING_NUMBER_INPUT_EXCEPTION);

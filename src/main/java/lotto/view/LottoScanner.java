2d7a342177bb1d46b3b6955b30503e9ffb090cc9
kkoon9 <rndrnjs2003@gmail.com>
2024-09-01 22:31:35 +0900

    feat: 로또 당첨 번호 입력하는 Scanner 개발

--- src/main/java/lotto/view/LottoScanner.java
+++ src/main/java/lotto/view/LottoScanner.java
@@ -1,6 +1,7 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import lotto.model.LottoWinningNumber;
 import lotto.model.PurchaseAmount;
 
 public class LottoScanner {
@@ -9,5 +10,10 @@ public class LottoScanner {
     public PurchaseAmount scanPurchaseAmount() {
         String input = Console.readLine();
         return new PurchaseAmount(Integer.parseInt(input));
+    }
+
+    public LottoWinningNumber scanLottoWinningNumber() {
+        String input = Console.readLine();
+        return new LottoWinningNumber(input);
     }
 }

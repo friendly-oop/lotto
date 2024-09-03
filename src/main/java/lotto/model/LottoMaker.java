99582ee09248cbbdb8f64c420d26c5459705b23c
kkoon9 <rndrnjs2003@gmail.com>
2024-09-03 21:12:25 +0900

    feat: 로또 구매 후 로또 번호 받은 로직 개발

---
+++ src/main/java/lotto/model/LottoMaker.java
@@ -0,0 +1,23 @@
+package lotto.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static lotto.Constant.LOTTO_NUMBER_COUNT;
+import static lotto.Constant.MAX_LOTTO_NUMBER;
+import static lotto.Constant.MIN_LOTTO_NUMBER;
+
+public class LottoMaker {
+    public List<Lotto> makeLottoList(PurchaseAmount amount) {
+        int lottoCount = amount.getLottoCount();
+
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            lottos.add(new Lotto(Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBER_COUNT)));
+        }
+
+        return lottos;
+    }
+}
--- src/main/java/lotto/model/PurchaseAmount.java
+++ src/main/java/lotto/model/PurchaseAmount.java
@@ -3,12 +3,17 @@ package lotto.model;
 import static lotto.Constant.PURCHASE_AMOUNT_INPUT_EXCEPTION;
 
 public class PurchaseAmount {
+    private static final int LOTTO_PRICE = 1000;
     private int amount;
 
     public PurchaseAmount(int amount) {
-        if (amount % 1000 != 0) {
+        if (amount % LOTTO_PRICE != 0) {
             throw new IllegalArgumentException(PURCHASE_AMOUNT_INPUT_EXCEPTION);
         }
         this.amount = amount;
+    }
+
+    public int getLottoCount() {
+        return amount / LOTTO_PRICE;
     }
 }
